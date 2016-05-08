8152c5b826001ff56a922fb65896ed44e81deeac
KonstantinPronin <prosayfer@mail.ru>
2016-05-08 20:31:54 +0300

    Update DZ.cpp

--- DZ.cpp
+++ DZ.cpp
@@ -4,21 +4,51 @@
 int main(){
 	
 	//while(std::cin){
-
+	City Spb;
 	try{
 	
-	City Spb;
+	
 	Spb.CreateElection();
-	Poll A(1, "south");
-//	A.ShowThis();
-//	A.ShowElectors();
-//	A.AddElector("Ivan", "Ivanov");
-//	A.ShowElectors();
-	//Elector First(1, "Ivan", "Ivanov");
-	//Elector Second(1, "Nik", "Ivanov");
+
+	Spb.AddPoll(1, "South");
+	Spb.AddPoll(2, "North");
+	Spb.AddPoll(3, "East");
+
+	Spb.AddElector(2, "Ivan", "Ivanov");
+ 	Spb.AddElector(1, "Misha", "Artemov");
+	Spb.AddElector(3, "Max", "Tiger");
+	Spb.AddElector(3, "Lena", "Kolina");
+	
 	Spb.ShowPolls();
-	//First.ShowThis();
-	//City::CreateElection();
+	Spb.ShowAllElectors();
+
+	Spb.Merge(1, 2);
+	Spb.ShowPolls();
+	Spb.ShowElectorsFromPoll(3);
+
+	Spb.AddCandidate(1, "Ivan", "Ivanov");
+	Spb.AddCandidate(3, "Lena", "Kolina");
+	Spb.DelCandidate("Lena", "Kolina");
+	Spb.AddCandidate(3, "Max", "Tiger");
+	Spb.ShowCandidates();
+
+	Spb.StartElection();
+	Spb.Vote(1, "Misha", "Artemov", "Max", "Tiger");
+	//Spb.Vote(1, "Ivan", "Ivanov", "Ivan", "Ivanov");
+	Spb.Vote(3, "Lena", "Kolina", "Max", "Tiger");
+	Spb.Vote(3, "Max", "Tiger", "Ivan", "Ivanov");
+	
+	Spb.FinishElection();
+	//Spb.Statistics(1);
+	Spb.AllStatistics();
+	
+	
+//	Spb.CloseElection();
+//	Spb.CreateElection();
+//	Spb.AddCandidate(3, "Lena", "Kolina");
+//	Spb.ShowPolls();
+//	Spb.ShowAllElectors();
+	
 	}catch(Error& e){
 		std::cout << e.what();
 	}
